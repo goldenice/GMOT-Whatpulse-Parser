94eb136f5dfac1e66b8d69f271a64513b49d98ed
joeykapi <joey@joeykapi.nl>
2016-05-10 20:51:28 +0200

    update heeft nu een timeout van 24 uur

--- classes/Update.class.php
+++ classes/Update.class.php
@@ -7,14 +7,20 @@
 class Update {
 	public function __construct() {}
 	public static function update() {
+		// Check if update has already been done last 24 hours
+		if (file_get_contents('lastupdate.txt') > time() - 86400 && isset($_GET['update']) == false)
+		{
+			return;
+		}
 		// Check if GitHub is up, if not, don't bother to update
-		echo "started\n";
+		echo "Started auto update\n";
 		$updateFiles = readExternalFile('https://raw.githubusercontent.com/goldenice/GMOT-Whatpulse-Parser/master/updatefiles.txt');
 		if ($updateFiles == false)
 		{
-			echo "updatefile does noet exist\n";
+			echo "GitHub is down\nStopping auto update";
 			return;
 		}
+		file_put_contents('lastupdate.txt', time());
 		$updateFiles = str_replace(array("\r", "\r\n"), "\n", $updateFiles);
 		$updateFiles = explode("\n", $updateFiles);
 		foreach ($updateFiles as $file)
@@ -26,7 +32,11 @@ class Update {
 				if (md5($new) != md5_file('../' . $file));
 				{
 					file_put_contents("_$file", $new);
-					echo "Added $file\n";
+					echo "Changed/added $file\n";
+				}
+				else
+				{
+					echo "$file has not been modified\n";
 				}
 			}
 		}
